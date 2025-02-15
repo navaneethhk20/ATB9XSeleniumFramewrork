cc03772d73dbe7c4d5555a311b0ba878b42d147f
navaneethhhk <hknavaneeth@gmail.com>
2025-02-15 21:26:22 +0530

    15th February-part4

---
+++ src/main/java/org/ATBSelenium/Pages/PageObjectModel/VWO/Improved_POM/LoginPage.java
@@ -0,0 +1,57 @@
+package org.ATBSelenium.Pages.PageObjectModel.VWO.Improved_POM;
+
+import org.ATBSelenium.base.CommonToAllPage;
+import org.ATBSelenium.utils.PropertiesReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends CommonToAllPage {
+    WebDriver driver;
+
+
+
+    // Page Class
+    // Page Locators
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+
+
+
+    // // Step 1 - Page Locators
+    private By username = By.id("login-username");
+    private By password = By.id("login-password");
+    private By signButton = By.id("js-login-btn");
+    private By error_message = By.id("js-notification-box-msg");
+
+    // If you are not using it , don't keep.
+    //private By singBySSO = By.xpath("//button[normalize-space()='Sign in using SSO']");
+
+
+    // Step 2 - Page Actions
+
+    public String loginToVWOLoginInvalidCreds(String user, String pwd) {
+        openVWOUrl();
+        enterInput(username,user);
+        enterInput(password,pwd);
+        clickElement(signButton);
+        presenceOfElement(error_message);
+        return getText(error_message);
+
+    }
+
+    public void loginToVWOLoginValidCreds(String user, String pwd) {
+        openVWOUrl();
+        enterInput(username,user);
+        enterInput(password,pwd);
+        clickElement(signButton);
+//        custom_wait();
+
+    }
+
+
+
+
+}
---
+++ src/test/java/org/ATBSelenium/tests/VWO/VWO_Improved_POM_PropertiesReaderTestCase.java
@@ -0,0 +1,43 @@
+package org.ATBSelenium.tests.VWO;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.ATBSelenium.Pages.PageObjectModel.VWO.Normal.DashBoardPage;
+import org.ATBSelenium.Pages.PageObjectModel.VWO.Normal.LoginPage;
+import org.ATBSelenium.base.CommonToAllTest;
+import org.ATBSelenium.driver.DriverManager;
+import org.ATBSelenium.utils.PropertiesReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VWO_Improved_POM_PropertiesReaderTestCase extends CommonToAllTest {
+    @Owner("Navaneeth")
+    @Description("Verify Negative test case")
+    @Test
+    public void testLoginNegativeVWO(){
+        LoginPage loginPageVWO = new LoginPage(DriverManager.getDriver());
+        String error_messege = loginPageVWO.loginToVWOLoginInvalidCreds(PropertiesReader.readKey("invalid_username"), PropertiesReader.readKey("invalid_password"));
+
+        assertThat(error_messege).isNotBlank().isNotNull().isNotEmpty();
+        Assert.assertEquals(error_messege, PropertiesReader.readKey("error_message"));
+    }
+    @Owner("Navaneeth")
+    @Description("Verify Positive test case")
+    @Test
+    public void testLoginPositiveVWO(){
+        LoginPage loginPage_VWO = new LoginPage(DriverManager.getDriver());
+        loginPage_VWO.loginToVWOLoginValidCreds(PropertiesReader.readKey("username"),PropertiesReader.readKey("password"));
+
+
+        DashBoardPage dashBoardPage  = new DashBoardPage(DriverManager.getDriver());
+        String usernameLoggedIn = dashBoardPage.loggedInUserName();
+
+
+        assertThat(usernameLoggedIn).isNotBlank().isNotNull().isNotEmpty();
+        Assert.assertEquals(usernameLoggedIn,PropertiesReader.readKey("expected_username"));
+    }
+}
