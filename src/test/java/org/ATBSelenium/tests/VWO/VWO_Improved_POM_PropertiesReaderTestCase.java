c314a4c42f7ac2723bf379fcf82c7387f355a8e1
navaneethhhk <hknavaneeth@gmail.com>
2025-02-15 21:54:52 +0530

    15th February-part5

--- src/test/java/org/ATBSelenium/tests/VWO/VWO_Improved_POM_PropertiesReaderTestCase.java
+++ src/test/java/org/ATBSelenium/tests/VWO/VWO_Improved_POM_PropertiesReaderTestCase.java
@@ -7,37 +7,61 @@ import org.ATBSelenium.Pages.PageObjectModel.VWO.Normal.LoginPage;
 import org.ATBSelenium.base.CommonToAllTest;
 import org.ATBSelenium.driver.DriverManager;
 import org.ATBSelenium.utils.PropertiesReader;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class VWO_Improved_POM_PropertiesReaderTestCase extends CommonToAllTest {
-    @Owner("Navaneeth")
-    @Description("Verify Negative test case")
-    @Test
-    public void testLoginNegativeVWO(){
-        LoginPage loginPageVWO = new LoginPage(DriverManager.getDriver());
-        String error_messege = loginPageVWO.loginToVWOLoginInvalidCreds(PropertiesReader.readKey("invalid_username"), PropertiesReader.readKey("invalid_password"));
 
-        assertThat(error_messege).isNotBlank().isNotNull().isNotEmpty();
-        Assert.assertEquals(error_messege, PropertiesReader.readKey("error_message"));
-    }
-    @Owner("Navaneeth")
-    @Description("Verify Positive test case")
+    private static final Logger logger = LogManager.getLogger(VWO_Improved_POM_PropertiesReaderTestCase.class);
+
+    @Owner("PRAMOD")
+    @Description("Verify that invalid creds give error message")
     @Test
-    public void testLoginPositiveVWO(){
+    public void testLoginNegativeVWO() {
+
+        logger.info("Starting the Testcases Page Object Model");
+
+
+        LoginPage loginPage_VWO = new LoginPage(DriverManager.getDriver());
+        String error_msg = loginPage_VWO.loginToVWOLoginInvalidCreds(PropertiesReader.readKey("invalid_username"),PropertiesReader.readKey("invalid_password"));
+
+
+        logger.info("End of the testLoginNegativeVWO !");
+
+        // Assertion AssertJ
+        assertThat(error_msg).isNotBlank().isNotNull().isNotEmpty();
+
+        // Assertion TestNG
+        Assert.assertEquals(error_msg,PropertiesReader.readKey("error_message"));
+
+
+    }
+
+    @Owner("PRAMOD")
+    @Description("Verify that valid creds dashboard page is loaded")
+    @Test
+    public void testLoginPositiveVWO() {
+
+        logger.info("Starting the Testcases |  Verify that valid creds dashboard page is loaded");
+
+
         LoginPage loginPage_VWO = new LoginPage(DriverManager.getDriver());
         loginPage_VWO.loginToVWOLoginValidCreds(PropertiesReader.readKey("username"),PropertiesReader.readKey("password"));
-
 
         DashBoardPage dashBoardPage  = new DashBoardPage(DriverManager.getDriver());
         String usernameLoggedIn = dashBoardPage.loggedInUserName();
 
 
+        logger.info("End and Asserting testLoginPositiveVWO");
+
         assertThat(usernameLoggedIn).isNotBlank().isNotNull().isNotEmpty();
         Assert.assertEquals(usernameLoggedIn,PropertiesReader.readKey("expected_username"));
+
+
+
     }
 }
